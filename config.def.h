f4f7fc9af27f6bb15dc0e28bd9cb2e16864117df
Itai <itai.cra@gmail.com>
2024-07-08 11:55:48 -0300

    Initial commit

--- config.def.h
+++ config.def.h
@@ -58,12 +58,22 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
-static const char *termcmd[]  = { "st", NULL };
+static const char *termcmd[] = {"terminator", NULL };
+static const char *browsercmd[] = {"floorp", NULL };
+static const char *vscodecmd[] = {"code", NULL };
+static const char *explorercmd[] = {"dolphin", NULL };
+static const char *rofidruncmd[] = { "rofi", "-show", "drun", NULL };
+static const char *rofiruncmd[] = { "rofi", "-show", "run", NULL };
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY|ShiftMask,             XK_b,	   spawn,          {.v = browsercmd } },
+	{ MODKEY|ShiftMask,             XK_v,      spawn,          {.v = vscodecmd } },
+	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = explorercmd } },
+	{ MODKEY|ShiftMask,              XK_r,      spawn,          {.v = rofiruncmd } },
+	{ MODKEY,                       XK_r,      spawn,          {.v = rofidruncmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
@@ -113,4 +123,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
