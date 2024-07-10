d236b629de46fada359657efd841a3d54c2ef119
Itai <itai.cra@gmail.com>
2024-07-10 15:25:57 -0300

    Changed explorer to thunar

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 6;        /* border pixel of windows */
-static const unsigned int gappx     = 4;        /* gaps between windows */
+static const unsigned int gappx     = 6;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -63,7 +63,7 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[] = {"terminator", NULL };
 static const char *browsercmd[] = {"floorp", NULL };
 static const char *vscodecmd[] = {"code", NULL };
-static const char *explorercmd[] = {"dolphin", NULL };
+static const char *explorercmd[] = {"thunar", NULL };
 static const char *rofidruncmd[] = { "rofi", "-show", "drun", NULL };
 static const char *rofiruncmd[] = { "rofi", "-show", "run", NULL };
 
